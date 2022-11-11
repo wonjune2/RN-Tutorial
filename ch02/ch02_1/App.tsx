af615e270f98420bf1ae235726fe0e00e4c5ac25
Joon <zzzdnjswns@naver.com>
2022-11-11 18:51:32 +0900

    react native init

---
+++ ch02/ch02_1/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {
+  Text,
+} from 'react-native';
+
+export default function App() {
+  const textElement = React.createElement(Text, null, 'Hello World!')
+  return textElement;
+}
