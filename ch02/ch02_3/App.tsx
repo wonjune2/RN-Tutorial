aa23d75b875f3b4310c1bb61c84f18a5f127937d
Joon <zzzdnjswns@naver.com>
2022-11-18 02:53:15 +0900

    functionComponetn Type

--- ch02/ch02_3/App.tsx
+++ ch02/ch02_3/App.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
 import ClassComponent from './src/screens/ClassComponent'
 import ArrowComponent from './src/screens/ArrowComponent'
+import Person from './src/screens/Person'
+import * as D from './src/data'
 
+const person = D.createRandomPerson()
 export default function App() {
   return (
     <SafeAreaView>
       <ClassComponent />
       <ArrowComponent />
+      <Person person={person} />
     </SafeAreaView>
   )
 }
