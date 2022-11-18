168ae8c13a98721aaa7d53b23a250f30329d6452
Joon <zzzdnjswns@naver.com>
2022-11-19 00:22:25 +0900

    ScrollView

--- ch02/ch02_3/App.tsx
+++ ch02/ch02_3/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { SafeAreaView, ScrollView } from 'react-native'
 import ClassComponent from './src/screens/ClassComponent'
 import ArrowComponent from './src/screens/ArrowComponent'
 import Person from './src/screens/Person'
 import * as D from './src/data'
-
+const people = D.makeArray(100).map(D.createRandomPerson);
 const person = D.createRandomPerson()
 export default function App() {
   return (
