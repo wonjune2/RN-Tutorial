d0c78142a0df977c964b6ed1dec038c465c85962
Joon <zzzdnjswns@naver.com>
2022-11-25 05:46:37 +0900

    styleSheet

--- ch03/ch03_1/App.tsx
+++ ch03/ch03_1/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>Hello world!</Text>
+    <SafeAreaView style={[styles.safeAreaView, {backgroundColor: 'blue'}]}>
+      <Text style={[styles.text, {color: 'white'}]}>Hello StyleSheet world!</Text>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  safeAreaView: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  text: {fontSize: 20}
+})
