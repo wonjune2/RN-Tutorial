1ea5a3559fe88bf738bb8edc097e3e7f29aa1a67
Joon <zzzdnjswns@naver.com>
2022-11-26 11:40:01 +0900

    flex

--- ch03/ch03_2/App.tsx
+++ ch03/ch03_2/App.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import {StyleSheet, SafeAreaView, Text} from 'react-native'
+import {StyleSheet, SafeAreaView, Text, Dimensions, Platform} from 'react-native'
+import { Colors } from "react-native-paper";
+
+const {width, height} = Dimensions.get('window')
 
 export default function App() {
   return (
     <SafeAreaView style={[styles.safeAreaView]}>
-      <Text style={[styles.text]}>Hello World!</Text>
+      <Text style={[styles.text]}>os: {Platform.OS}</Text>
+      <Text style={[styles.text]}>width: {width}</Text>
+      <Text style={[styles.text]}>height: {height}</Text>
     </SafeAreaView>
   )
 }
 
 const styles= StyleSheet.create({
-  safeAreaView: {},
-  text: {}
+  safeAreaView: {backgroundColor: Colors.blue500, flex: 1},
+  text: {fontSize: 20, Color: Colors.blue200}
 })
