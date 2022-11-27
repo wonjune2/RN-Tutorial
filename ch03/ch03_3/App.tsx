81628fd744ba349743be24fc800fae4c8388c37d
Joon <zzzdnjswns@naver.com>
2022-11-27 09:30:33 +0900

    Image

--- ch03/ch03_3/App.tsx
+++ ch03/ch03_3/App.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import {StyleSheet, SafeAreaView} from "react-native"
+import {StyleSheet, SafeAreaView, ImageBackground, Image} from "react-native"
+import * as D from './src/data'
 
+const avatarUrl = D.randomAvatarUrl()
+const avatarSize = 50
+console.log(avatarUrl)
 export default function App() {
-  return <SafeAreaView style={styles.flex}/>
+  return (
+  <SafeAreaView style={styles.flex}>
+    <ImageBackground style={[styles.flex, styles.backgroundImage]}
+      source={require('./src/assets/images/bg.jpg')}>
+      <Image source={{uri: avatarUrl}} style={[styles.image]} />
+    </ImageBackground>
+  </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-  flex: {flex: 1}
+  flex: {flex: 1},
+  backgroundImage: {flex: 1},
+  image: {width: avatarSize, height: avatarSize, borderRadius: avatarSize / 2},
+
 })
--- ch03/ch03_3/src/data/util.ts
+++ ch03/ch03_3/src/data/util.ts
@@ -1,7 +1,7 @@
-export const makeArray = (length: number) => new Array(length).fill(null);
+export const makeArray = (length: number) => new Array(length).fill(null)
 export const random = (min: number, max: number): number =>
-  Math.round(Math.random() * (max - min)) + min;
+  Math.round(Math.random() * (max - min)) + min
 export const unsplashUrl = (width: number, height: number): string =>
-  `https://source.unsplashUrl.com/random/${width}X${height}`;
+  `https://source.unsplash.com/random/${width}x${height}`
 export const avatarUriByName = (name: string) =>
-  `https://ur-avatars.com/api/?name=${name.split(' ').join('+')}`;
+  `https://ui-avatars.com/api/?name=${name.split(' ').join('+')}`
