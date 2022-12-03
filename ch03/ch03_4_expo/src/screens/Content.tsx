037ae1a2f480e748a8c0fef9e509c51a92b6681c
Joon <zzzdnjswns@naver.com>
2022-12-03 23:47:32 +0900

    scrollView

--- ch03/ch03_4_expo/src/screens/Content.tsx
+++ ch03/ch03_4_expo/src/screens/Content.tsx
@@ -1,31 +1,34 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 import { Colors } from "react-native-paper";
 import * as D from '../data'
 
 const title = 'Content'
+const avatars = D.makeArray(200).map((notUsed) => D.randomAvatarUrl())
+
 export default function CopyMe() {
+    const children = avatars.map((avatarUrl, index) => (
+        <View key={index.toString()} style={styles.avatarView} >
+            <Image style={styles.avatar} source={{uri: avatarUrl}}/>
+        </View>
+    ))
     return (
         <View style={[styles.view]}>
-            <Text style={[styles.text]}>{title}</Text>
-            <View style={[{flex: 0, backgroundColor: Colors.red500}]}>
-                <Text>flex: 0</Text>
-            </View>
-            <View style={[{flex: 1, backgroundColor: Colors.green500}]}>
-                <Text>flex: 1</Text>
-            </View>
-            <View style={[{flex: 2, backgroundColor: Colors.purple500}]}>
-                <Text>flex: 2</Text>
-            </View>
+            {children}
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     view: {
+        flexDirection: 'row',
+        // overflow: 'hidden',
+        flexWrap: 'nowrap',
+        justifyContent: 'center',
         flex: 1,
-        padding: 5, 
-        backgroundColor: Colors.blue900
+        padding: 5
     },
-    text: {fontSize: 20, color: 'white'}
+    text: {fontSize: 20},
+    avatarView: {padding: 3},
+    avatar: {width: 50, height: 50, borderRadius: 25}
 })
