7be3f7416316fb1f894e066b9e754954b230bf86
Joon <zzzdnjswns@naver.com>
2022-12-04 19:42:10 +0900

    init

---
+++ gilbut/LearReactNative/App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello React!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
