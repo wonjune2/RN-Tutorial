cb42f4dfb8fa17b1c0ef2c54707a4cdbfe7f4f01
Joon <zzzdnjswns@naver.com>
2022-12-15 05:42:22 +0900

    DayLog

---
+++ gilbut/DayLog/App.js
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native'
+import RootStack from './screens/RootStack'
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  )
+}
---
+++ gilbut/DayLog/screens/FeedsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+
+function FeedsScreens() {
+  return <View style={styles.block} />
+}
+
+const styles = StyleSheet.create({
+  block: {},
+})
+
+export default FeedsScreens
