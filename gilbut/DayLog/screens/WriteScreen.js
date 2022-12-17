70c89a0aee702683345e8b2178eb6d83c73981ec
Joon <zzzdnjswns@naver.com>
2022-12-17 09:15:37 +0900

    useRef

--- gilbut/DayLog/screens/WriteScreen.js
+++ gilbut/DayLog/screens/WriteScreen.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import WriteEditor from '../component/WriteEditor'
 import WriteHeader from '../component/WriteHeader'
 
 function WriteScreen() {
   return (
     <SafeAreaView style={styles.block}>
       <WriteHeader />
+      <WriteEditor />
     </SafeAreaView>
   )
 }
