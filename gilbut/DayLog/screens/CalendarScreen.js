374270bef1b31b41f3232e97d79aa006a29c89a7
Joon <zzzdnjswns@naver.com>
2022-12-19 06:17:01 +0900

    spring

--- gilbut/DayLog/screens/CalendarScreen.js
+++ gilbut/DayLog/screens/CalendarScreen.js
@@ -1,21 +1,63 @@
-import React, { useContext } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import { Animated, StyleSheet, Text, View, Button } from 'react-native'
 import LogContext from '../contexts/LogContext'
 
+function FadeInAndOut() {
+  const animation = useRef(new Animated.Value(1)).current
+
+  const [enabled, setEnabled] = useState(false)
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: enabled ? 1 : 0,
+      useNativeDriver: true,
+    }).start()
+  }, [enabled, animation])
+  return (
+    <View>
+      <Animated.View
+        style={[
+          styles.rectangle,
+          {
+            transform: [
+              {
+                translateX: animation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, 150],
+                }),
+              },
+            ],
+            opacity: animation.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 0],
+            }),
+          },
+        ]}
+      />
+      <Button
+        title="Toggle"
+        onPress={() => {
+          setEnabled(!enabled)
+        }}
+      />
+    </View>
+  )
+}
+
 function CalendarScreen() {
-  const { text } = useContext(LogContext)
   return (
     <View style={styles.block}>
-      <Text style={styles.text}>text: {text}</Text>
+      <FadeInAndOut />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   block: {},
-  text: {
-    padding: 16,
-    fontSize: 24,
+  rectangle: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'black',
   },
 })
 
--- gilbut/DayLog/screens/FeedsScreen.js
+++ gilbut/DayLog/screens/FeedsScreen.js
@@ -1,13 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { StyleSheet, View, TextInput } from 'react-native'
+import FeedList from '../component/FeedList'
 import FloatingWriteButton from '../component/FloatingWriteButton'
 import LogContext from '../contexts/LogContext'
 
 function FeedsScreen() {
   const { logs } = useContext(LogContext)
+  const [hidden, setHidden] = useState(false)
+  const onScrolledToBottom = (isBottom) => {
+    if (hidden !== isBottom) {
+      setHidden(isBottom)
+    }
+  }
+
   return (
     <View style={styles.block}>
-      <FloatingWriteButton />
+      <FeedList logs={logs} onScrolledToBottom={onScrolledToBottom} />
+      <FloatingWriteButton hidden={hidden} />
     </View>
   )
 }
