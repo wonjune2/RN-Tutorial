e1986d545310c728e572fd3391f21e4b373bfc58
Joon <zzzdnjswns@naver.com>
2023-01-04 22:55:23 +0900

    getposts limit

--- gilbut/PublicGalleryJoon/lib/posts.js
+++ gilbut/PublicGalleryJoon/lib/posts.js
@@ -2,6 +2,8 @@ import {
   collection,
   doc,
   getDocs,
+  limit,
+  orderBy,
   serverTimestamp,
   setDoc,
 } from 'firebase/firestore'
@@ -18,8 +20,14 @@ export function createPost({ user, photoURL, description }) {
   })
 }
 
+export const PAGE_SIZE = 3
+
 export async function getPosts() {
-  const querySnapshot = await getDocs(collection(db, 'posts'))
+  const querySnapshot = await getDocs(
+    collection(db, 'posts'),
+    orderBy('createAt', 'desc'),
+    limit(PAGE_SIZE)
+  )
   const posts = querySnapshot.docs.map((doc) => ({
     id: doc.id,
     ...doc.data(),
