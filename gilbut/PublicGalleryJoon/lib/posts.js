bf1ce5f2f11723909d2a0517fcb9134d2d15870e
Joon <zzzdnjswns@naver.com>
2023-01-06 03:34:17 +0900

    posts modify

--- gilbut/PublicGalleryJoon/lib/posts.js
+++ gilbut/PublicGalleryJoon/lib/posts.js
@@ -1,10 +1,12 @@
 import {
   collection,
   doc,
+  endBefore,
   getDoc,
   getDocs,
   limit,
   orderBy,
+  query,
   serverTimestamp,
   setDoc,
   startAfter,
@@ -26,32 +28,59 @@ export function createPost({ user, photoURL, description }) {
 export const PAGE_SIZE = 3
 
 export async function getPosts() {
-  const querySnapshot = await getDocs(
+  const first = query(
     collection(db, 'posts'),
     orderBy('createAt', 'desc'),
     limit(PAGE_SIZE)
   )
+  const querySnapshot = await getDocs(first)
   const posts = querySnapshot.docs.map((doc) => ({
     id: doc.id,
     ...doc.data(),
   }))
-
   return posts
 }
 
 export async function getOlderPosts(id) {
-  const cursorDoc = await getDoc(doc(db, 'posts', id))
-  const snapshot = await getDocs(
-    collection(db, 'posts'),
-    orderBy('createAt', 'desc'),
-    limit(PAGE_SIZE),
-    startAfter(cursorDoc)
-  )
+  const docRef = doc(db, 'posts', id)
+  const cursorDoc = await getDoc(docRef)
+  console.log(cursorDoc.exists())
+  if (cursorDoc.exists()) {
+    const first = query(
+      collection(db, 'posts'),
+      orderBy('createAt', 'desc'),
+      startAfter(cursorDoc),
+      limit(PAGE_SIZE)
+    )
+    const snapshot = await getDocs(first)
+    const posts = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }))
+    return posts
+  }
+  return []
+}
 
-  const posts = snapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }))
+export async function getNewerPosts(id) {
+  const docRef = doc(db, 'posts', id)
+  const cursorDoc = await getDoc(docRef)
+  if (cursorDoc.exists()) {
+    const first = query(
+      collection(db, 'posts'),
+      orderBy('createAt', 'desc'),
+      endBefore(cursorDoc),
+      limit(PAGE_SIZE)
+    )
+    const snapshot = await getDocs(first)
 
-  return posts
+    const posts = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }))
+
+    return posts
+  }
+
+  return []
 }
--- gilbut/PublicGalleryJoon/screens/FeedScreen.js
+++ gilbut/PublicGalleryJoon/screens/FeedScreen.js
@@ -1,17 +1,22 @@
+import { async } from '@firebase/util'
 import React, { useEffect, useState } from 'react'
-import { ActivityIndicator, FlatList, View } from 'react-native'
+import {
+  ActivityIndicator,
+  FlatList,
+  RefreshControl,
+  StyleSheet,
+} from 'react-native'
 import PostCard from '../components/PostCard'
-import { getOlderPosts, getPosts, PAGE_SIZE } from '../lib/posts'
+import { getNewerPosts, getOlderPosts, getPosts, PAGE_SIZE } from '../lib/posts'
 
 function FeedScreen() {
   const [posts, setPosts] = useState(null)
-
+  // 마지막 포스트까지 조회했음을 명시하는 상태
   const [noMorePost, setNoMorePost] = useState(false)
-  useEffect(() => {
-    getPosts().then(setPosts)
-  })
+  const [refreshing, setRefreshing] = useState(false)
 
   useEffect(() => {
+    // 컴포넌트가 처음 마운트될 때 포스트 목록을 조회한 후 `posts` 상태에 담기
     getPosts().then(setPosts)
   }, [])
 
@@ -19,12 +24,27 @@ function FeedScreen() {
     if (noMorePost || !posts || posts.length < PAGE_SIZE) {
       return
     }
+
     const lastPost = posts[posts.length - 1]
     const olderPosts = await getOlderPosts(lastPost.id)
     if (olderPosts.length < PAGE_SIZE) {
       setNoMorePost(true)
     }
-    setPosts(posts.councat(olderPosts))
+    setPosts(posts.concat(olderPosts))
+  }
+
+  const onRefresh = async () => {
+    if (!posts || posts.length === 0 || refreshing) {
+      return
+    }
+    const firstPost = posts[0]
+    setRefreshing(true)
+    const newerPosts = await getNewerPosts(firstPost.id)
+    setRefreshing(false)
+    if (newerPosts.length === 0) {
+      return
+    }
+    setPosts(newerPosts.concat(posts))
   }
 
   return (
@@ -40,13 +60,16 @@ function FeedScreen() {
           <ActivityIndicator style={styles.spinner} size={32} color="#6200ee" />
         )
       }
+      refreshControl={
+        <RefreshControl onRefresh={onRefresh} refreshing={refreshing} />
+      }
     />
   )
 }
 
 const renderItem = ({ item }) => (
   <PostCard
-    createAt={item.createAt}
+    createdAt={item.createdAt}
     description={item.description}
     id={item.id}
     user={item.user}
